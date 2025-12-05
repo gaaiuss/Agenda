29a9177c8920f255b0103c0f06af5482a13ce38a
gaaiuss <caio.gui.castro@gmail.com>
2025-12-05 10:51:59 -0300

    creating form fields

--- contact/forms.py
+++ contact/forms.py
@@ -5,9 +5,36 @@ from .models import Contact
 
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'Type your first',
+            },
+        ),
+        label='Name',
+        help_text='Help text for user'
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.fields['first_name'].widget.attrs.update({
+        #     'class': 'classe-a classe-b',
+        #     'placeholder': 'Type your first',
+        # })
+
     class Meta:
         model = Contact
-        fields = 'first_name', 'last_name', 'phone',
+        fields = ('first_name', 'last_name', 'phone',)
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': 'classe-a classe-b',
+        #             'placeholder': 'Type your first name',
+        #         }
+        #     ),
+        # }
 
     def clean(self):
         # cleaned_data = self.cleaned_data
