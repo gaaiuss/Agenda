86759738f88657e1e8754212c578efca92bd7888
gaaiuss <caio.gui.castro@gmail.com>
2025-12-10 14:23:43 -0300

    user register form

--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 from .models import Contact
@@ -52,4 +53,29 @@ class ContactForm(forms.ModelForm):
 
 
 class RegisterForm(UserCreationForm):
-    ...
+    first_name = forms.CharField(
+        required=True,
+        min_length=2,
+    )
+    last_name = forms.CharField(required=True,)
+    email = forms.EmailField(required=True,)
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError(
+                    'E-mail address already exists', code='invalid'
+                )
+            )
+
+        return email
