9a47293d6e6376fab40fbb5afb0bfe9a7dbc4915
gaaiuss <caio.gui.castro@gmail.com>
2025-12-10 14:45:49 -0300

    messages

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
 
 from contact.forms import RegisterForm
 
@@ -6,11 +7,18 @@ from contact.forms import RegisterForm
 def register(request):
     form = RegisterForm()
 
+    # messages.info(request, 'Any text')
+    # messages.success(request, 'Any text')
+    # messages.warning(request, 'Any text')
+    # messages.error(request, 'Any text')
+
     if request.method == 'POST':
         form = RegisterForm(request.POST)
 
         if form.is_valid():
             form.save()
+            messages.success(request, 'User registered')
+            return redirect('contact:index')
 
     return render(
         request,
