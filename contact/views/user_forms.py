f5f6b899d480d41752fb10a96e372939af930c47
gaaiuss <caio.gui.castro@gmail.com>
2025-12-11 14:08:12 -0300

    login and logout

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,4 +1,5 @@
-from django.contrib import messages
+from django.contrib import auth, messages
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import redirect, render
 
 from contact.forms import RegisterForm
@@ -18,7 +19,7 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'User registered')
-            return redirect('contact:index')
+            return redirect('contact:login')
 
     return render(
         request,
@@ -27,3 +28,30 @@ def register(request):
             'form': form,
         }
     )
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'Login success')
+            return redirect('contact:index')
+        messages.error(request, 'Invalid login')
+
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form,
+        }
+    )
+
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
